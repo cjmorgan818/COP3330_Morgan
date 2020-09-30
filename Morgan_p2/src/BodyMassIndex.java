22e82d73228e3f9c53704fb267566c7e99b10748
Christopher Morgan <cjmorgan818@knights.ucf.edu>
2020-09-29 22:26:38 -0400

    updated code

--- Morgan_p2/src/BodyMassIndex.java
+++ Morgan_p2/src/BodyMassIndex.java
@@ -1,2 +1,84 @@
+import java.util.Scanner;
+
 public class BodyMassIndex {
+    private double weight;
+    private double height;
+    private double bmi;
+    private int choice;
+    private double userHeight;
+    private double userWeight;
+    private boolean moreInput;
+
+    public void moreInput(){
+
+    }
+    public BodyMassIndex(double userHeight, double userWeight) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            // Getting the input entered by the user
+            System.out.print("Enter Weight (in Pounds):");
+            this.userWeight = userWeight;
+
+            System.out.print("Enter Height (in inches):");
+            this.userHeight = userHeight;
+
+            break;
+        }
+    }
+
+    public boolean isUserInput() {
+        return moreInput;
+    }
+
+    public void setUserInput(boolean userInput) {
+        this.moreInput = moreInput;
+    }
+
+
+    public double getUserHeight() {
+        return userHeight;
+    }
+
+    public double getUserWeight() {
+        return userWeight;
+    }
+
+    public void setUserHeight(double userHeight) {
+        this.userHeight = userHeight;
+    }
+
+    public void setUserWeight(double userWeight) {
+        this.userWeight = userWeight;
+    }
+
+
+    public void calculateBMI() {
+        if (choice == 1) {
+
+            bmi = (weight * 703) / (height * height);
+
+        } else {
+            bmi = weight / (height * height);
+
+        }
+    }
+
+    public void displayBmi() {
+        String category = "";
+        if (bmi < 18.5)
+            category = "UnderWeight";
+        else if (bmi >= 18.5 && bmi <= 24.9)
+            category = "Normal";
+        else if (bmi >= 25 && bmi <= 29.9)
+            category = "OverWeight";
+        else if (bmi >= 30)
+            category = "Obese";
+        System.out.println("YOur BMI:" + bmi);
+        System.out.println("You are " + category);
+
+    }
 }
+
+
+
--- Morgan_p2/src/BodyMassIndexTest.java
+++ Morgan_p2/src/BodyMassIndexTest.java
@@ -1,2 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BodyMassIndexTest {
+
+    @org.junit.jupiter.api.Test
+    void readUserData() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateBMI() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void displayBmi() {
+    }
 }
